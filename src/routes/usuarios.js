02c6ab12c4a0bb459bc4e2bdf072a79d383966a8
Fernando Aliaga <fernandoach333@gmail.com>
2024-11-22 11:20:17 -0500

    OK: Register

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { crearUsuario } from '../services/crearUsuario.js'
+import { listarUsuarioPorEmail } from '../services/listarUsuarioPorEmail.js'
 
 const usuariosRoutes = express.Router()
 
@@ -14,9 +15,14 @@ usuariosRoutes.get('/register', (req, res) => {
 usuariosRoutes.post('/register', async (req, res) => {
   try {
     const { nombreCompleto, genero, email, passwd, repasswd } = req.body
-    console.log(req.body)
-    await crearUsuario(nombreCompleto, genero, email, passwd, repasswd)
-    return res.redirect('/')
+    // comprobar que el usuario no existe
+    const userExist = await listarUsuarioPorEmail(email)
+    const findIndex = userExist.findIndex(o => o.email === email)
+    if (findIndex === -1) {
+      await crearUsuario(nombreCompleto, genero, email, passwd, repasswd)
+      return res.redirect('/usuarios/login')
+    }
+    return res.redirect('/usuarios/register')
   } catch (error) {
     return res.json(error)
   }
